5b15796c5dffb0e76f242dc71f2107eb4509afbe
florian-marquier <florianmarquier1@gmail.com>
2023-09-25 14:24:10 +0200

    exo4

---
+++ exo4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int calcul(int a, int b)
+{
+    return (a*b);
+}
+int calcul(int a, int b, int c)
+{
+    return (a*b*c);
+}
+int main()
+{
+    int x = 10, y =30, z = 50;
+    cout<<calcul(x,y)<<" "<<calcul(x,y,z)<<endl;
+    return 0;
+}
